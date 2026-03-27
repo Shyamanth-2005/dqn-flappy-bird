aa4708e9f3f8f08af0913d0c9688c24ae49b1d77
Shyamanth-2005 <shyamanthreddy.m@gmail.com>
2026-03-27 19:20:43 +0530

    feat: implement target network + optimizer + loss function

--- agent.py
+++ agent.py
@@ -1,10 +1,11 @@
 import yaml
 import torch
 import random
-import gymnasium
 import itertools
-import flappy_bird_gymnasium
+import gymnasium
 from dqn import DQN
+from torch import nn
+import flappy_bird_gymnasium
 from experience_replay import ReplayMemory
 """
 ACTIONS SPACE:
@@ -24,15 +25,51 @@ class Agent:
   def __init__(self,hyperparameter_set):
     with open('hyperparameters.yaml', 'r') as file:
       all_hyperparameter_sets = yaml.safe_load(file)
-      hyperparameter = all_hyperparameter_sets[hyperparameter_set]
+      hyperparameters = all_hyperparameter_sets[hyperparameter_set]
       # print(hyperparameter)
       
-      self.replay_memory_size = hyperparameter['replay_memory_size'] # size of the replay memory
-      self.mini_batch_size = hyperparameter['mini_batch_size'] # size of the mini batch for training
-      self.epsilon_init = hyperparameter['epsilon_init'] # 1 = 100 % random action (full exploration)
-      self.epsilon_decay = hyperparameter['epsilon_decay'] # decay rate of epsilon per episode
-      self.epsilon_min = hyperparameter['epsilon_min'] # minimum value of epsilon (minimum exploration)
+      self.replay_memory_size = hyperparameters['replay_memory_size'] # size of the replay memory
+      self.mini_batch_size = hyperparameters['mini_batch_size'] # size of the mini batch for training
+      self.epsilon_init = hyperparameters['epsilon_init'] # 1 = 100 % random action (full exploration)
+      self.epsilon_decay = hyperparameters['epsilon_decay'] # decay rate of epsilon per episode
+      self.epsilon_min = hyperparameters['epsilon_min'] # minimum value of epsilon (minimum exploration)
+      
+      self.network_sync_rate = hyperparameters['network_sync_rate'] # no of steps (rate) to copy the next policy network to the target network 
+      self.learning_rate_a = hyperparameters['learning_rate_a'] # learning rate -> how fast or slow the model learns
+      self.discount_factor_g = hyperparameters['discount_factor_g'] # decides how imp future rewards are
+      
+      
+      self.loss_fn = nn.MSELoss()
+      self.optimizer = None
   
+  def optimize(self, mini_batch, policy_dqn, target_dqn):
+    """ 
+    q learning formula
+    q[state, action]  = q[state, action] + learning+rate * (reward + discount_factor * max(q[next_state, action]) - a[state, action])
+    
+    dqn target formula
+    q[state, action] = reward if next_state is terminal else
+    reward + discount_factor * max(q[next_state,actions])
+    """
+    
+    for state, action, next_state, reward, terminated in mini_batch:
+      if terminated:
+        target = reward
+      else:
+        with torch.no_grad():
+          target_q = reward + self.discount_factor_g * target_dqn(next_state).max()
+      
+      current_q = policy_dqn(state)
+      
+      # compute loss
+      loss = self.loss_fn(current_q, target_q)
+      
+      # optimize the model
+      self.optimizer.zero_grad() # clear gradients
+      self.backward() # compute gradients (backwardpropagation)
+      self.optimizer.step() # update network parameters ie. weights and bais
+      
+    
   def run(self, is_training = True, render = False):
     #env = gymnasium.make("FlappyBird-v0", render_mode="human" if render else None, use_lidar=False) # use_lidar: [False, True]
     env = gymnasium.make("CartPole-v1", render_mode="human" if render else None)
@@ -51,7 +88,20 @@ class Agent:
       
       epsilon = self.epsilon_init
       
-    
+      # tagret network for better estimates
+      # why two networks when policy network trains we copy a set of 
+      # it to the target network so we can stablize the training (instead of moving targets)
+      # and get better results
+      target_dqn = DQN(num_states, num_actions).to(DEVICE)
+      target_dqn.load_state_dict(policy_dqn.state_dict())
+      
+      # track number of steps taken , used for syncing policy => target network
+      step_count = 0
+      
+      # policy network optimizer , "Adam" optimizer
+      self.optimizer = torch.optim.Adam(policy_dqn.parameters(), lr = self.learning_rate_a)
+      
+      
     for episode in itertools.count():
       state, _ = env.reset()
       state = torch.tensor(state, dtype = torch.float, device = DEVICE)
@@ -88,15 +138,36 @@ class Agent:
           reward = torch.tensor(reward, dtype = torch.float, device = DEVICE)
           
           if is_training:
+            # save experience into memory
             memory.append((state, action, next_state, reward, terminated))
-          
+
+            # increment step counter
+            step_count += 1
           # move to next state
           state = next_state
       
+      # keep track of the rewards collected per episode
       rewards_per_episode.append(episode_reward)
      
       epsilon = max(epsilon * self.epsilon_decay, self.epsilon_min)
       epsilon_history.append(epsilon)
+      
+      # if enough experience has been collected
+      if len(memory) > self.mini_batch_size:
+        
+        # sample from memory
+        mini_batch = memory.sample(self.mini_batch_size)
+        
+        self.optimize(mini_batch, policy_dqn, target_dqn)
+        
+        # copy policy network to target network after a certain number of steps
+        if step_count > self.network_sync_rate:
+          target_dqn.load_state_dict(policy_dqn.state_dict())
+          step_count = 0
+        
+      
+      
+      
 
 if __name__ == "__main__":
   agent = Agent("cartpole1")
